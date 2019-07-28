52da627cdd7fd65b84b81b77c0d0afc59bdb2f19
hey-cube <tales.phan.x@gmail.com>
2019-07-28 18:16:21 +0900

    関数は別ファイルに切り出し

--- src/components.jsx
+++ src/components.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { calculateWinner } from "./utils";
 
 function Square(props) {
   return (
@@ -114,26 +115,4 @@ export class Game extends React.Component {
       </div>
     );
   }
-}
-
-// ========================================
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import { CLICK_SQUARE, JUMP_TO_PAST } from "./actions";
+import { calculateWinner } from "./utils";
 
 const initialState = {
   history: [
@@ -44,25 +45,3 @@ function game(state = initialState, action) {
 }
 
 export const app = combineReducers({ game });
-
-// ========================================
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
