032bbe0366af029bc338861ac9305f2ee8798121
hey-cube <tales.phan.x@gmail.com>
2019-07-28 18:06:14 +0900

    store 追加

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
 import { Game } from "./components";
+import { app } from "./reducers";
 import "./index.css";
 
+const store = createStore(app);
 ReactDOM.render(<Game />, document.getElementById("root"));
