a78a1ca55a267fe6968427ce8c68f608ff7b2f64
hey-cube <tales.phan.x@gmail.com>
2019-08-19 21:27:37 +0900

    ログをコンソールに表示する middleware を自作

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import { app } from "./reducers";
 import { GameContainer } from "./containers";
+import { logger } from "./middlewares";
 import "./index.css";
 
-const store = createStore(app);
+const store = createStore(app, applyMiddleware(logger));
 
 render(
   <Provider store={store}>
---
+++ src/middlewares.js
@@ -0,0 +1,7 @@
+export const logger = store => next => action => {
+  console.log("previous state", store.getState());
+  console.log("dispatching", action);
+  let result = next(action);
+  console.log("next state", store.getState());
+  return result;
+};
